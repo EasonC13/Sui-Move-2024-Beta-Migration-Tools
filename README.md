b5e023f4031fea4554e89170ab22d52db3c898f9
EasonC13 <eason.tw.chen@gmail.com>
2024-04-07 18:21:14 -0400

    udpate

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Sui-Move-2024-Beta-Migration-Tools
+
+# How to use it?
+
+1. Go to the root directory (where Move.toml located) of your Move project.
+2. Run migrate_vector.sh
---
+++ migrate_vector.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Check if Move.toml exists in the current directory
+if [ ! -f "./Move.toml" ]; then
+    echo "Move.toml file not found in the current directory."
+    exit 1
+fi
+
+echo "Have you committed all changes and there are no uncommitted changes? (yes/no)"
+read USER_CONFIRMATION
+
+if [ "$USER_CONFIRMATION" != "yes" ]; then
+    echo "Please commit all changes before running this script."
+    exit 1
+fi
+
+# Process all .move files in the ./source directory
+find ./sources -type f -name "*.move" | while read FILENAME; do
+    # Perform the replacement using more compatible syntax
+    # and ensure no unnecessary spaces are included in the replacement
+    sed -i.bak -E 's/vector::([a-zA-Z_]+)\((&mut |&)?([a-zA-Z_]+),\s*(.*)\)/\3.\1(\4)/g' "$FILENAME"
+    
+    if [ $? -eq 0 ]; then
+        echo "Processing completed for $FILENAME."
+        # Delete the backup file after successful processing
+        rm "${FILENAME}.bak"
+    else
+        echo "An error occurred during processing $FILENAME. Restoring the original file."
+        # Restore the original file from backup and remove the .bak file
+        mv "${FILENAME}.bak" "$FILENAME"
+        echo "Original file has been restored for $FILENAME. Please check your file for errors."
+    fi
+done
